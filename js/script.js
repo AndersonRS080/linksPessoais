1fa28fe9fe69c0724cd391f91b94767ce97f9942
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-09-19 10:50:38 -0300

    atualização

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 1500,
+  
+}).reveal(`li , footer`, { interval: 100 })
+
+ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 1500,
+  
+}).reveal(`.container`)
